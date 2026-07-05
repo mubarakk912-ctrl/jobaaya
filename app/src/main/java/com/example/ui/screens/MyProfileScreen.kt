0743c1f9ba0280cfea0aea2e0cb846623f673a54
mubarakk912-ctrl <Mubarakk912@gmail.com>
2026-07-06 00:50:44 +0530

    tii done code

--- app/src/main/java/com/example/ui/screens/MyProfileScreen.kt
+++ app/src/main/java/com/example/ui/screens/MyProfileScreen.kt
@@ -152,28 +152,34 @@ fun MyProfileScreen(
                         AndroidView(
                             factory = { ctx ->
                                 val view = LayoutInflater.from(ctx).inflate(R.layout.layout_contact_info, null)
-                                
-                                // Comprehensive Global Country Codes List
-                                val spinnerCountryCode = view.findViewById<Spinner>(R.id.spinnerCountryCode)
-                                val globalCountryCodes = arrayOf(
-                                    "+1", "+7", "+20", "+27", "+30", "+31", "+32", "+33", "+34", "+36", "+39", "+40", "+41", "+43", "+44", "+45", "+46", "+47", "+48", "+49", 
-                                    "+51", "+52", "+53", "+54", "+55", "+56", "+57", "+58", "+60", "+61", "+62", "+63", "+64", "+65", "+66", "+81", "+82", "+84", "+86", "+90", 
-                                    "+91", "+92", "+93", "+94", "+95", "+98", "+211", "+212", "+213", "+216", "+218", "+220", "+221", "+222", "+223", "+224", "+225", "+226", 
-                                    "+227", "+228", "+229", "+230", "+231", "+232", "+233", "+234", "+235", "+236", "+237", "+238", "+239", "+240", "+241", "+242", "+243", 
-                                    "+244", "+245", "+248", "+249", "+250", "+251", "+252", "+253", "+254", "+255", "+256", "+257", "+258", "+260", "+261", "+262", "+263", 
-                                    "+264", "+265", "+266", "+267", "+268", "+269", "+290", "+291", "+297", "+298", "+299", "+350", "+351", "+352", "+353", "+354", "+355", 
-                                    "+356", "+357", "+358", "+359", "+370", "+371", "+372", "+373", "+374", "+375", "+376", "+377", "+378", "+380", "+381", "+382", "+383", 
-                                    "+385", "+386", "+387", "+389", "+420", "+421", "+423", "+500", "+501", "+502", "+503", "+504", "+505", "+506", "+507", "+509", "+590", 
-                                    "+591", "+592", "+593", "+594", "+595", "+596", "+597", "+598", "+599", "+670", "+672", "+673", "+674", "+675", "+676", "+677", "+678", 
-                                    "+679", "+680", "+681", "+682", "+683", "+685", "+686", "+687", "+688", "+689", "+690", "+691", "+692", "+850", "+852", "+853", "+855", 
-                                    "+856", "+880", "+886", "+960", "+961", "+962", "+963", "+964", "+965", "+966", "+967", "+968", "+970", "+971", "+972", "+973", "+974", 
-                                    "+975", "+976", "+977", "+992", "+993", "+994", "+995", "+996", "+998"
-                                ).distinct().sortedBy { it.substring(1).toInt() }.toTypedArray()
 
-                                val adapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_item, globalCountryCodes)
-                                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
-                                spinnerCountryCode.adapter = adapter
-                                
+                                val spinnerCountry = view.findViewById<Spinner>(R.id.spinnerCountry)
+                                val tvCountryCodeDisplay = view.findViewById<android.widget.TextView>(R.id.tvCountryCodeDisplay)
+
+                                // Map of Country Names and their respective Dialing Codes
+                                val countryData = linkedMapOf(
+                                    "India" to "+91", "United States" to "+1", "United Kingdom" to "+44",
+                                    "UAE" to "+971", "Australia" to "+61", "Canada" to "+1",
+                                    "Germany" to "+49", "France" to "+33", "Japan" to "+81"
+                                )
+                                val countryNames = countryData.keys.toTypedArray()
+
+                                val countryAdapter = ArrayAdapter(ctx, android.R.layout.simple_spinner_dropdown_item, countryNames)
+                                spinnerCountry.adapter = countryAdapter
+
+                                // Dynamic Sync: Country select karne par code apne aap badlega
+                                spinnerCountry.onItemSelectedListener = object : android.widget.AdapterView.OnItemSelectedListener {
+                                    override fun onItemSelected(parent: android.widget.AdapterView<*>?, view: android.view.View?, position: Int, id: Long) {
+                                        val selectedCountry = countryNames[position]
+                                        val matchingCode = countryData[selectedCountry]
+                                        tvCountryCodeDisplay.text = matchingCode
+
+                                        // Force white text visibility in dropdown selection
+                                        (view as? android.widget.TextView)?.setTextColor(android.graphics.Color.WHITE)
+                                    }
+                                    override fun onNothingSelected(parent: android.widget.AdapterView<*>?) {}
+                                }
+
                                 view
                             },
                             modifier = Modifier.fillMaxWidth()
